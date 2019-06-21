858d2cb67f748f2923e925a7a5ef7bea54a2be1a
Martin Connor <martinconnor@Martins-MacBook-Pro-3.local>
2019-06-21 08:00:10 -0700

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist/');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
